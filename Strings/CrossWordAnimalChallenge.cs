ac4ba8bdcfbe2799cfb017ebc1dabf215a576789
Tahirid1812210 <cs1812210@szabist.pk>
2024-06-04 23:52:53 +0300

    started working on crossword challenge

---
+++ Strings/CrossWordAnimalChallenge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsOfProgrammingTasks.Strings
+{
+    internal class CrossWordAnimalChallenge
+    {
+        public static readonly string[] animalNames =
+        {
+            "cat", "dog", "elephant", "tiger", "lion", "bear", "zebra", "giraffe", "hippo", "rhino"
+        };
+
+
+        public static bool isAnimal(string word)
+        {
+            foreach(var animal in animalNames)
+            {
+                if(animal.Equals(word , StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+
+                
+            }
+            return false;
+        }
+
+    }
+}
